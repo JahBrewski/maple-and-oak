b03092f6bfb6eaea2d644a5c78d085be0ad5fa87
Joel Brewer <joelbrewer01@gmail.com>
2013-12-12 15:47:19 -0600

    add support for rspec and initialize first tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Maple and Oak'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Maple and Oak')
+    end
+  end
+end
