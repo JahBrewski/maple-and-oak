e8e00f11cb19d645774c74f75c285c0feff69a8c
Joel Brewer <joelbrewer01@gmail.com>
2013-12-12 12:08:57 -0600

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TnBusinessConnection::Application.config.secret_key_base = '31581988e182bce94cf00c7595d2f8185514f73323fc7b086e3cad24c4d53902ccfb1faf8ddf565b7256c995f181838833ef6771ca14ce509f1cfdea93ad1949'
