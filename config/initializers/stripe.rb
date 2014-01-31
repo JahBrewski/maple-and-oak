036aeb1cb14f78c0465ad0129951e00ed8d8be72
Joel Brewer <joelbrewer01@gmail.com>
2014-01-31 16:57:04 -0600

    clean up stripe initializer. add biz plans to profile browsing page

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,2 @@
-stripe = { app_id: ENV['STRIPE_APP_ID'], secret: ENV['STRIPE_SECRET'] }
-STRIPE_PUBLIC_KEY = stripe[:app_id]
-Stripe.api_key    = stripe[:secret]
+STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
+Stripe.api_key    = ENV['STRIPE_API_KEY'] 
