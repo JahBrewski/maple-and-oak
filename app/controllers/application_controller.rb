0bd2e31c82d3a07af935f3e1ae1fe11fb0530590
Joel Brewer <joelbrewer01@gmail.com>
2014-02-05 14:56:46 -0600

    update devise flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def investor_only
     unless current_user.investor?
       redirect_to root_path
     end
+  end
+
+  def after_sign_in_path_for(user)
+    profile_path
+  end
+
+  def after_sign_up_path_for(user)
+    profile_path  
   end
 
   protected
