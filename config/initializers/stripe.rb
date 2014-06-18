66fe875b1259836481708e0c0979a8e736b98e1a
Joel Brewer <joelbrewer01@gmail.com>
2014-06-17 20:45:12 -0500

    add charge.failed webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,25 +1,13 @@
 class ChargeFailed
   def call(event)
-  end
-end
-
-class BillingEventLogger
-  def initialize(logger)
-    @logger = logger
-  end
-
-  def call(event)
-    subscription = Subscription.where(:stripe_customer_token => event.customer)
-    customer = User.find(subscription.user.id)
-    @logger.info "BILLING:#{event.type}:#{event.id}"
-    @logger.info "BILLING CUSTOMER:#{customer}"
+    subscription = Subscription.where(:stripe_customer_token => event.data.object.card.customer)
+    customer = User.find(subscription[0].user.id)
+    customer.update_attribute(:active_subscription, false)
   end
 end
 
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 Stripe.api_key    = ENV['STRIPE_API_KEY'] 
 StripeEvent.configure do |events|
-  events.all BillingEventLogger.new(Rails.logger)
   events.subscribe 'charge.failed', ChargeFailed.new
 end
-
